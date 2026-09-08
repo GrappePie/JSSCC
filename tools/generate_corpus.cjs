60a4c47dbf43f1e9e66277e679971740e45e0300
GrappePie <grappepie@gmail.com>
2026-09-08 03:42:55 -0600

    Make CI fail on piped test failures and align probe timings with their manifest

--- tools/generate_corpus.cjs
+++ tools/generate_corpus.cjs
@@ -20,17 +20,17 @@ for (const velocity of [32, 64, 96, 127]) save('velocity-' + velocity + '.mid', 
 for (let n = 35; n <= 81; n++) save('drum-' + n + '.mid', note({channel: 9, note: n, velocity: 100, duration: 960}),
   'Drum channel10, note' + n + ', velocity100; NoteOff at1s');
 for (const [controller, name] of [[7, 'volume'], [11, 'expression'], [10, 'pan']]) save('cc-' + name + '.mid', smf([[
-  ...ev(0, 0xc0, 16), ...ev(0, 0x90, 60, 100), ...ev(960, 0xb0, controller, 0),
-  ...ev(960, 0xb0, controller, 127), ...ev(960, 0x80, 60, 0), ...ev(480, 0xff, 0x2f, 0)
+  ...ev(0, 0xc0, 16), ...ev(0, 0x90, 60, 100), ...ev(1920, 0xb0, controller, 0),
+  ...ev(1920, 0xb0, controller, 127), ...ev(1920, 0x80, 60, 0), ...ev(480, 0xff, 0x2f, 0)
 ]]), 'Held organ6s; CC' + controller + '=0 at2s and127 at4s');
 save('hold-release.mid', smf([[
   ...ev(0, 0xc0, 16), ...ev(0, 0xb0, 64, 127), ...ev(0, 0x90, 60, 100),
-  ...ev(960, 0x80, 60, 0), ...ev(960, 0xb0, 64, 0), ...ev(480, 0xff, 0x2f, 0)
+  ...ev(1920, 0x80, 60, 0), ...ev(1920, 0xb0, 64, 0), ...ev(480, 0xff, 0x2f, 0)
 ]]), 'Hold1 on; key release at2s; pedal off at4s');
 save('bend-12.mid', smf([[
   ...ev(0, 0xc0, 16), ...ev(0, 0xb0, 101, 0), ...ev(0, 0xb0, 100, 0), ...ev(0, 0xb0, 6, 12),
-  ...ev(0, 0x90, 69, 100), ...ev(960, 0xe0, 127, 127), ...ev(960, 0xe0, 0, 64),
-  ...ev(960, 0x80, 69, 0), ...ev(480, 0xff, 0x2f, 0)
+  ...ev(0, 0x90, 69, 100), ...ev(1920, 0xe0, 127, 127), ...ev(1920, 0xe0, 0, 64),
+  ...ev(1920, 0x80, 69, 0), ...ev(480, 0xff, 0x2f, 0)
 ]]), 'RPN0,0 bend12; A4, maxbend at2s, centered at4s, off at6s');
 fs.writeFileSync(path.join(out, 'manifest.json'), JSON.stringify(manifest, null, 2) + '\n');
 console.log(JSON.stringify({midiFiles: manifest.files.length, directory: out, originalWavs: 0}));
