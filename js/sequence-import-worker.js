d013ecb5dc6f0ac78bc213a12f7e0f2710fee491
GrappePie <grappepie@gmail.com>
2026-09-08 18:49:16 -0600

    Bust sequence codec cache for volume compatibility fix

--- js/sequence-import-worker.js
+++ js/sequence-import-worker.js
@@ -1,6 +1,6 @@
 /* Conversion stays off the UI thread; input is data, never executable code. */
 'use strict';
-importScripts('./sequence-codec.js?v=os-midi-1');
+importScripts('./sequence-codec.js?v=os-midi-2');
 self.onmessage=({data})=>{
   try {
     const result=self.JSSCCSequenceCodec.convert(data.bytes,{id:data.id,title:data.title});
